20077eeb7edb051a3942eb4891cd7e062ce4937a
Aditya Sharma <ads.as2020@gmail.com>
2025-11-10 00:09:09 +0530

    Add game assets and implement playable frontend and backend

--- script.js
+++ script.js
@@ -1,50 +1,69 @@
+const stone = document.querySelector(".stone");
+const paper = document.querySelector(".paper");
+const scissor = document.querySelector(".scissor");
+const container = document.querySelector(".container");
+const containerImages = document.querySelector(".container .images");
+const scoreUser = document.querySelector(".score .user p");
+const scoreBot = document.querySelector(".score .bot p");
 
-let computerScore=0;
-let humanScore=0;
+let para = document.createElement("p");
+para.style.margin = "0px";
+let botChoiceImg = document.createElement("img");
+botChoiceImg.classList.add("choice-img");
+let userChoiceImg = document.createElement("img");
+userChoiceImg.classList.add("choice-img");
 
-function getComputerChoice(){
-    let cc = Math.random();
-    if(cc<0.33) return "rock";
-    else if(cc>0.33 && cc<0.66 ) return "paper";
-    else if(cc>0.33) return "sizor";
+let computerScore = 0;
+let humanScore = 0;
+
+function getComputerChoice() {
+	let cc = Math.random();
+	if (cc < 0.33) return "stone";
+	else if (cc > 0.33 && cc < 0.66) return "paper";
+	else if (cc > 0.33) return "scissor";
 }
 
-function getHumanChoice(){
-    let hc = parseInt(prompt("Enter 1 for rock,2 for paper 3,for sizor"));
-    if(hc===1) return "rock";
-    if(hc===2) return "paper";
-    if(hc===3) return "sizor";
+function playRound(humanChoice, computerChoice) {
+	if (humanChoice === computerChoice) {
+		para.innerText = `It's a tie! you both chose ${humanChoice}`;
+	} else if (
+		(humanChoice === "stone" && computerChoice === "scissor") ||
+		(humanChoice === "paper" && computerChoice === "stone") ||
+		(humanChoice === "scissor" && computerChoice === "paper")
+	) {
+		humanScore++;
+		para.innerText = `You win ${humanChoice} beats ${computerChoice}`;
+	} else {
+		computerScore++;
+		para.innerText = `You lose ${computerChoice} beats ${humanChoice}`;
+	}
+	botChoiceImg.src = "assets/" + computerChoice + ".webp";
+	userChoiceImg.src = "assets/" + humanChoice + ".webp";
+	containerImages.appendChild(userChoiceImg);
+	containerImages.appendChild(botChoiceImg);
+	container.appendChild(para);
+	scoreUser.innerHTML = `Your <br> Score <br> ${humanScore}`;
+	scoreBot.innerHTML = `vIllager <br> Score <br> ${computerScore}`;
 }
 
-function playRound(humanChoice,computerChoice){
-    if(humanChoice===computerChoice){
-        return "It's a tie!";
-    }
-    else if(humanChoice==="rock" && computerChoice==="sizor" ||
-            humanChoice==="paper" && computerChoice==="rock" ||
-            humanChoice==="sizor" && computerChoice==="paper"){
-                humanScore++;
-                return `You win ${humanChoice} beats ${computerChoice}`;
-    }
-    else{
-        computerScore++;
-        return `You lose ${computerChoice} beats ${humanChoice}`;
-    }
-}
+stone.addEventListener("click", () => {
+	humanChoice = "stone";
+	playRound(humanChoice, getComputerChoice());
+});
+paper.addEventListener("click", () => {
+	humanChoice = "paper";
+	playRound(humanChoice, getComputerChoice());
+});
+scissor.addEventListener("click", () => {
+	humanChoice = "scissor";
+	playRound(humanChoice, getComputerChoice());
+});
 
-function playGame(){
-    while(humanScore<5 && computerScore<5){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(humanSelection, computerSelection));
-        console.log(`Human: ${humanScore} Computer: ${computerScore}`);
-    }
-    if(humanScore===5){
-        console.log("Congratulations! You won the game!");
-    }
-    else{
-        console.log("Sorry! The computer won the game!");
-    }
+if (computerScore === 5 || humanScore === 5) {
+	if (computerScore > humanScore) {
+		para.innerText = "Game Over! Villager Wins!";
+	} else {
+		para.innerText = "Game Over! You Win!";
+	}
+	container.appendChild(para);
 }
-
-playGame();
